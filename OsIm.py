8972fe89509b24bb77ebf6b57fa61008769aad10
Oscar de Felice <oscar.defelice@linkalab.it>
2019-11-19 16:50:52 +0100

    ImageComparator class updated

--- OsIm.py
+++ OsIm.py
@@ -30,6 +30,8 @@
 ### 17/11/2019 - Oscar: image comparator class - match method added.
 ### 18/11/2019 - Oscar: image class - added sift as default feature detection model.
 ### 18/11/2019 - Oscar: image comparator class - match method updated with model selection.
+### 19/11/2019 - Oscar: image comparator class - match method updated with knn methods.
+### 19/11/2019 - Oscar: image comparator class - added orb algorithm for feature detection.
 ###
 
 ### import Libraries ###
@@ -121,8 +123,10 @@ class Image:
             model = sift
         elif model_name == 'surf':
             model = surf
+        elif model_name == 'orb':
+            model = orb
         else:
-            raise ValueError('The only implemented models are sift and surf.')
+            raise ValueError('The only implemented models are sift, surf and orb.')
 
         return model
 
@@ -133,6 +137,7 @@ class Image:
 
             The argument model_name is a string the name of the model.
             Default value is 'sift'.
+            Admitted values are 'sift', 'surf' and 'orb'.
 
             returns a tuple of lists.
             keypoints is a list of keypoint objects.
@@ -230,13 +235,28 @@ class ImageComparator:
 
             It takes two objects of type Image as input.
             The model_name argument indicates the model to use to calculate images keypoints.
-            It returns a list of matches objects.
+            It returns a list of match objects.
         """
         matches = self.match_model_.match(Image_1.keypoints(model_name)[1],
                                           Image_2.keypoints(model_name)[1])
         matches = sorted(matches, key = lambda x:x.distance)
         return matches
+        
+    def knnmatch(self, Image_1, Image_2, model_name = DEFAULT_FEATURE_MODEL, k=2):
+        """
+            match method.
 
+            It takes two objects of type Image as input.
+            The model_name argument indicates the model to use to calculate images keypoints.
+            k (default 2) argument is an int and indicates the number of classes to collect the matches.
+            It returns a list of couples of match objects.
+        """
+        matches = self.match_model_.knnMatch(Image_1.keypoints(model_name)[1],
+                                          Image_2.keypoints(model_name)[1], k=2)
+        matches = sorted(matches, key = lambda x:np.abs(x[0].distance - x[1].distance))
+        
+        return matches
+    
     def score(self, Image_1, Image_2, model_name = DEFAULT_FEATURE_MODEL):
         """
             score of the similarity.
@@ -249,17 +269,17 @@ class ImageComparator:
         """
 
         return score
-
-    def __matches_to_plot(self, Image_1, Image_2,
-                          model_name = DEFAULT_FEATURE_MODEL,
-                          n_matches = N_MATCHES_PLOT):
+        
+    def __matches_to_plot_classic(self, Image_1, Image_2,
+                                  model_name = DEFAULT_FEATURE_MODEL,
+                                  n_matches = N_MATCHES_PLOT):
         """
             Private method to choose how many matches to plot.
 
             It takes two optional arguments.
             The model_name argument indicates the model to use to calculate images keypoints.
             An int n_matches will give the number of matches.
-            A float between 0 and 1 will give all the matches whose score is greater than the argument.
+            A float greater than 0 will give all the matches whose score is greater than the argument.
         """
         all_matches = self.match(Image_1, Image_2, model_name)
 
@@ -267,31 +287,114 @@ class ImageComparator:
             return all_matches[:n_matches]
         except ValueError:
             try:
-                if (n_matches > 1 or n_matches < 0):
-                    raise ValueError('n_matches can only be an integer or a float between 0 and 1')
+                if (n_matches < 0):
+                    raise ValueError('n_matches can only be an integer or a positive float')
                 
                 good_matches = [match for match in all_matches if match.distance >= n_matches]
                 return good_matches
             except:
-                raise ValueError('%s is not an integer or a float' %n_matches)
+                raise ValueError('%s is not an integer nor a float' %n_matches)
+                
+    def __matches_to_plot_knn(self, Image_1, Image_2,
+                              model_name = DEFAULT_FEATURE_MODEL,
+                              n_matches = None):
+        """
+            Private method to choose how many matches to plot.
+
+            It makes use of knnmatch method.
+            It takes two optional arguments.
+            The model_name argument indicates the model to use to calculate images keypoints.
+            An int n_matches will give the number of matches.
+            A float greater than 0 will give all the matches whose score is greater than the argument.
+            
+            returns a dictionary of drawing parameters.
+        """
+        all_matches = self.knnmatch(Image_1, Image_2, model_name, k = 2)
+           
+        try:
+            self.__ratio_test(all_matches, threshold)
+
+            draw_params = dict(matchColor = (0,255,0),
+                              singlePointColor = (255,0,0),
+                              matchesMask = matchesMask[:n_matches],
+                              flags = cv2.DrawMatchesFlags_DEFAULT)
+        except:
+            self.__ratio_test(all_matches)
+
+            draw_params = dict(matchColor = (0,255,0),
+                               singlePointColor = (255,0,0),
+                               matchesMask = matchesMask,
+                               flags = cv2.DrawMatchesFlags_DEFAULT)
+            
+        return draw_params
+                
+    def __matches_to_plot(self, Image_1, Image_2, knnmatch == True,
+                          model_name = DEFAULT_FEATURE_MODEL,
+                          n_matches = N_MATCHES_PLOT, threshold = LOWE_THRS):
+        """
+            Private method to choose how many matches to plot.
+
+            It takes three optional arguments.
+            knnmatch is a boolean argument (default True), and it is True if one wants to use Lowe's ratio test.
+            The model_name argument indicates the model to use to calculate images keypoints.
+            An int n_matches will give the number of matches.
+            A float greater than 0 will give all the matches whose score is greater than the argument.
+        """
+        try:
+            return self.__matches_to_plot_knn(Image_1, Image_2,
+                                              model_name = DEFAULT_FEATURE_MODEL,
+                                              n_matches = n_matches,
+                                              threshold = LOWE_THRS)
+        except:
+            return self.__matches_to_plot_classic(Image_1, Image_2,
+                                                  model_name = DEFAULT_FEATURE_MODEL,
+                                                  n_matches = n_matches)
+            
 
     def plot_matching(self, Image_1, Image_2,
+                      keypoints_1 = None, keypoints_2 = None,
                       model_name = DEFAULT_FEATURE_MODEL,
                       figsize = DEFAULT_FIGSIZE,
-                      n_matches = N_MATCHES_PLOT):
+                      n_matches = N_MATCHES_PLOT,
+                      threshold = LOWE_THRS):
         """
             method to plot the images with feature matching.
 
             The model_name argument indicates the model to use to calculate images keypoints.
             figsize is a tuple tuning the plot size.
             n_matches can be int and will give the number of matches to show.
-            n_matches can be a float between 0 and 1 and the function will show all the matches
+            n_matches can be a positive float and the function will show all the matches
             whose score is greater than the n_matches.
         """
-        img_to_plot = cv2.drawMatches(Image_1.img_, Image_1.keypoints(model_name)[0],
-                                      Image_2.img_, Image_2.keypoints(model_name)[0],
-                                      self.__matches_to_plot(Image_1, Image_2, model_name, n_matches),
-                                      Image_2.img_, flags=2)
-
+        
+        try:
+            img_to_plot = cv2.drawMatches(Image_1.img_, keypoints_1,
+                                          Image_2.img_, keypoints_2,
+                                          self.__matches_to_plot(Image_1, Image_2, model_name, n_matches, threshold),
+                                          Image_2.img_, flags=2)
+        except:
+            img_to_plot = cv2.drawMatches(Image_1.img_, Image_1.keypoints(model_name)[0],
+                                          Image_2.img_, Image_2.keypoints(model_name)[0],
+                                          self.__matches_to_plot(Image_1, Image_2, model_name, n_matches, threshold),
+                                          Image_2.img_, flags=2)
+        
         plt.figure(figsize=figsize)
         plt.imshow(img_to_plot), plt.show()
+    
+    def __ratio_test(self, matches, threshold)
+        """
+            Private method to calculate the ratio test and in Lowe's paper defining SIFT.
+            
+            It takes the list of couple of matches as one argument.
+            As second argument the threshold value.
+            
+            It returns a list of lists, being the mask for plotting matches.
+        """
+        matchesMask = [[0,0] for i in range(len(matches))]
+        
+        # ratio test as per Lowe's paper
+        for i,(m,n) in enumerate(matches):
+            if m.distance < threshold*n.distance:
+                matchesMask[i]=[1,0]
+                
+        return matchesMask
