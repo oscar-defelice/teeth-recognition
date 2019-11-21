a45a3ece7df5cbd7b815d0191612cb4b749a9138
Oscar de Felice <oscar.defelice@linkalab.it>
2019-11-21 19:38:59 +0100

    Creation of a script extracting the most similar image to a given one

---
+++ Corps-recognition.py
@@ -0,0 +1,27 @@
+###
+### Corps-recognition.py
+###
+### Created by Oscar de Felice on 21/11/2019.
+### Copyright © 2019 Oscar de Felice.
+###
+### This program is free software: you can redistribute it and/or modify
+### it under the terms of the GNU General Public License as published by
+### the Free Software Foundation, either version 3 of the License, or
+### (at your option) any later version.
+###
+### This program is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+### GNU General Public License for more details.
+###
+### You should have received a copy of the GNU General Public License
+### along with this program. If not, see <http://www.gnu.org/licenses/>.
+###
+########################################################################
+###
+### Corps-recognition.py
+### This is the script to find the most similar image to a given one. It makes use of OsIm module.
+###
+### 21/11/2019 - Oscar: creation of this script.
+###
+###
