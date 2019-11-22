384bc4ce446e5488c2ef93962b4131f5551034e8
Oscar de Felice <oscar.defelice@linkalab.it>
2019-11-22 12:48:06 +0100

    Script for comparing a series of images

--- Corps-recognition.py
+++ Corps-recognition.py
@@ -20,8 +20,60 @@
 ########################################################################
 ###
 ### Corps-recognition.py
-### This is the script to find the most similar image to a given one. It makes use of OsIm module.
+### This is the script to find the most similar image to a given one.
+### It makes use of OsIm module.
 ###
-### 21/11/2019 - Oscar: creation of this script.
+### 21/11/2019 - Oscar: Creation of this script.
+### 22/11/2019 - Oscar: Instantiate Image objects.
 ###
 ###
+
+### Import libraries ###
+import OsIm
+import cv2
+import glob
+
+### Model selection
+model = 'surf'
+model_compare = 'bf'
+
+### Constants of the script
+images_dir = 'path/to/images_dir'
+img_zero_path = 'path/to/img_zero'
+
+### List of image paths
+images_path = glob.glob(images_dir)
+
+### Instantiate Image objects
+image_zero = OsIm.Image(img_zero_path) # Training image
+
+images = {}
+for i, image in enumerate(images_path):
+    images[i] = OsIm.Image(image) # Test images
+
+### Find keypoints using the model selected
+image_zero.find_keypoints(model_name = model)
+
+for image in images.values():
+    image.find_keypoints(model_name = model)
+
+### Comparator object istance
+comparator = OsIm.ImageComparator(matcher = model_compare)
+
+### Compute matches and scores
+scores = []
+for image in images.values():
+    comparator.knnmatch(image_zero, image, model_name = model)
+    scores.append(comparator.score())
+
+### Find the maximum score
+maximum_score = max(scores)
+maximum_index = scores.index(maximum_score)
+
+print('We found an image with score %.1f' %maximum_score)
+
+### Plot the image with maximum similarity score
+comparator.plot_matching(image_zero, images[maximum_index])
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
