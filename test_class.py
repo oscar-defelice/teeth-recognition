f73f2c782cee9dcd38a37a32757d1325e96dc5d1
Oscar de Felice <oscar.defelice@linkalab.it>
2019-11-25 10:16:46 +0100

    Comments added.

--- test_class.py
+++ test_class.py
@@ -1,2 +1,32 @@
+###
+### test_class.py
+###
+### Created by Oscar de Felice on 21/11/2019.
+### Copyright © 2019 Oscar de Felice.
+###
+### This program is free software: you can redistribute it and/or modify
+### it under the terms of the GNU General Public License as published by
+### the Free Software Foundation, either version 3 of the License, or
+### (at your option) any later version.
+###
+### This program is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+### GNU General Public License for more details.
+###
+### You should have received a copy of the GNU General Public License
+### along with this program. If not, see <http://www.gnu.org/licenses/>.
+###
+########################################################################
+###
+### test_class.py
+### This is the script collecting test classes for the OsIm library.
+### It makes use of OsIm module.
+###
+### 22/11/2019 - Oscar: Creation of this script.
+### 25/11/2019 - Oscar: Comments added.
+###
+###
+
 def test_placeholder():
     pass
